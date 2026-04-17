5ecdd51fe794ccd115b34b2dbbc0959de1206e36
Valerii <vallerus55@gmail.com>
2026-04-17 21:04:24 +0200

    add shadow

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
     kotlin("plugin.serialization") version "2.0.21"
     application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.example"
