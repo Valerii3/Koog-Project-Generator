85540a97378cc64fb3f0b5c092c7563b648f4bb4
Valerii <vallerus55@gmail.com>
2026-04-19 13:36:39 +0200

    remove legacy

--- src/main/kotlin/com/example/kooggen/template/GeneratedFile.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kooggen.template
-
-data class GeneratedFile(
-    val path: String,
-    val content: String
-)
--- src/main/kotlin/com/example/kooggen/template/ProjectTemplate.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kooggen.template
-
-import com.example.kooggen.model.AgentTemplateType
-import com.example.kooggen.model.ProjectSpec
-
-interface ProjectTemplate {
-    val type: AgentTemplateType
-    fun render(spec: ProjectSpec): List<GeneratedFile>
-}
